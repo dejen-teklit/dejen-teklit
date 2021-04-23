4d3043284f15389f29312b26dfec55dbabb846f8
dejen-teklit <dejenteklit@outlook.com>
2021-04-23 11:57:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+$(Document).ready(function(){
+    $(".button").click(function(){
+      var value=$(this).attr("data-filter");
+      if(value == "all"){
+        $(".filter").show("100");
+      }
+      else{
+        $(".filter").not("."+value).hide("1000");
+        $(".filter").filter("."+value).show("1000");
+      }
+      $("ul .button").click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+      })
+    })
+  })
